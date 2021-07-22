66dc7bbc83c03832090532d2dfca9a47a3d66dad
shi-n <shi-n_xyz@cloudsquare.jp>
2021-07-22 14:50:35 +0900

    1st

--- ExampleSwiftUIPencilKit/ContentView.swift
+++ ExampleSwiftUIPencilKit/ContentView.swift
@@ -6,16 +6,38 @@
 //
 
 import SwiftUI
+import PencilKit
 
 struct ContentView: View {
+    @State var pkcanvasview: PKCanvasView = PKCanvasView()
+
+    let screenSize: CGSize = UIScreen.main.bounds.size
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("template")
+                .resizable()
+                .scaledToFit()
+                .frame(width: screenSize.width, height: screenSize.height, alignment: .center)
+            PencilKitCanvasView(pkcanvasview: self.$pkcanvasview)
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct PencilKitCanvasView: UIViewRepresentable {
+    @Binding var pkcanvasview: PKCanvasView
+    let pktoolpicker = PKToolPicker()
+
+    func makeUIView(context: Context) -> PKCanvasView {
+        self.pkcanvasview.isOpaque = false
+        self.pkcanvasview.backgroundColor = .clear
+        self.pkcanvasview.overrideUserInterfaceStyle = .light
+        self.pktoolpicker.addObserver(pkcanvasview)
+        self.pktoolpicker.setVisible(true, forFirstResponder: pkcanvasview)
+        self.pkcanvasview.becomeFirstResponder()
+        return pkcanvasview
+    }
+    
+    func updateUIView(_ uiView: PKCanvasView, context: Context) {
     }
 }
