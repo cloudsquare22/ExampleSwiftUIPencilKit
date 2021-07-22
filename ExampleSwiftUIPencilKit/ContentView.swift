68053229925230c14158ad23f2a8947d27f98dd5
shi-n <shi-n_xyz@cloudsquare.jp>
2021-07-22 13:42:08 +0900

    Initial Commit

---
+++ ExampleSwiftUIPencilKit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExampleSwiftUIPencilKit
+//
+//  Created by Shin Inaba on 2021/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExampleSwiftUIPencilKit/ExampleSwiftUIPencilKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleSwiftUIPencilKitApp.swift
+//  ExampleSwiftUIPencilKit
+//
+//  Created by Shin Inaba on 2021/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ExampleSwiftUIPencilKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
